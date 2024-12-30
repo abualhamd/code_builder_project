1c2bac156fdb6ef35c0c7b2f87865e7e63da21d5
abualhamd <53705516+abualhamd@users.noreply.github.com>
2024-12-30 22:28:55 +0200

    initial commit

---
+++ bin/code_builder_project.dart
@@ -0,0 +1,5 @@
+import 'package:code_builder_project/code_builder_project.dart' as code_builder_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_builder_project.calculate()}!');
+}
---
+++ test/code_builder_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_builder_project/code_builder_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
